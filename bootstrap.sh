2ced4cae8080c65842ed2384c5929e6a9f2ab7f5
baloghd <balogh.domonkos@gmail.com>
2019-12-28 14:12:59 +0100

    conda

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 sudo apt update
 
+# anaconda
+
+# https://repo.anaconda.com/archive/ innen scrapelni a legfelső Linux-x86_64.sh-t
+
+
 # brave 
 sudo apt install apt-transport-https curl
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
@@ -7,7 +12,14 @@ echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable ma
 sudo apt update
 sudo apt install brave-browser
 
-
+# utilities
 sudo apt install vim-gtk keepassxc transmission ssh
 snap install code --classic
 snap install docker
+
+# git setup
+
+git config --global user.email "balogh.domonkos@gmail.com"
+git config --global user.name "baloghd"
+git config --global credential.helper store
+
